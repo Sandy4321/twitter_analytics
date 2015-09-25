87f6a690ae626fd298f5116c1691b677f1cdc6b5
Emmanuel Wang <clochard93@gmail.com>
2015-09-25 18:59:12 -0400

    right output of English words for t.s.

--- autorun.sh
+++ autorun.sh
@@ -9,7 +9,7 @@ fi
 
 echo "OK, collect data for $required_time second(s)..."
 python twitterstream.py $required_time > output.txt 
-python tweet_sentiment.py AFINN-111.txt output.txt 
+python tweet_sentiment.py AFINN-111.txt output.txt > tsout.txt
 python term_sentiment.py AFINN-111.txt output.txt 
 python frequency.py output.txt 
 python top_ten.py output.txt 
--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python
+# coding=ASCII
+
+import re
 import sys
 import json
 
+# use ASCII to filter those non-english texts
+reload(sys)
+sys.setdefaultencoding('ascii')
+
+def iter_dic(name):
+    print name.items()
 
 def create_score(sf):
     afinnfile = sf
@@ -10,45 +20,35 @@ def create_score(sf):
         term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
 
-    return scores
-    #print scores.items() # Print every (term, score) pair in the dictionary
-    
-def return_score(text, scores):
-    # search scores in text, which is translated to unicode, then output score
+def get_scores(text):
     this_score = 0
     for pair in scores.items():
-#        print type(unicode(pair[0], encoding="utf-8"))
-        if text.find(unicode(pair[0], encoding="utf-8")) != -1:
+        if text.find(pair[0]) != -1:
             this_score += pair[1]
-
     return this_score
-    
 
-def get_tf(tf, scores):
-    # read line form tweet_file
-#    i = 0
+def get_texts(tf):
+    global text_scores
+    text_scores = {}
+
     for line in tf:
-        this_score = 0
-        line_deco = json.loads(line)
-        if u'delete' in line_deco:
-            print this_score
+        try:
+            line_deco = json.loads(line)
+            this_text = str(line_deco[u'text'])
+            this_score = get_scores(this_text)
+            print this_text, " ", this_score
+#            print this_text
+
+        except:
+            # TODO: print a "NULL" to non-English text
             continue
-        text = line_deco[u'text']
-        #print text
-        
-        # calculate the score
-        this_score = return_score(text, scores)
-        print this_score
-#        i += 1
-#        if i == 6:
-#            break
 
 def hw(sf, tf):
     # add AFINN-111.txt to a dictionary called scores
-    scores = create_score(sf)
+    create_score(sf)
     
-    # TODO: read & clean & score tweet_file
-    get_tf(tf, scores)
+    # TODO: optimize & get correct outputs
+    get_texts(tf)
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -57,6 +57,7 @@ def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     hw(sent_file, tweet_file)
+    iter_dic(text_scores)
 #    lines(sent_file)
 #    lines(tweet_file)
 
